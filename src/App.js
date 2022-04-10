3b9f8d899e99cc69e33c0f77486548e7a4c92e1a
Abhijeet9242 <87429783+Abhijeet9242@users.noreply.github.com>
2022-04-10 19:43:45 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import HomeDisplay from "./components/HomeDisplay";
-import Home from "./components/Home";
+// import HomeDisplay from "./components/HomeDisplay";
+// import Home from "./components/Home";
 // import Song from "./components/Song";
 import Myroutes from "./components/Myroutes";
 
 function App() {
   return (
     <div className="App">
-      {/* <HomeDisplay />
-      <Home /> */}
       <Myroutes />
     </div>
   );
